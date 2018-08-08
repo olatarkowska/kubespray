11122bd72a212adb829f3149e90048a3e10137c1
wikiselev <vk6@sanger.ac.uk>
2018-08-08 10:13:48 +0100

    get ready to deploy on farm4-head1

--- inventory/my-k8s-cluster/cluster.tf
+++ inventory/my-k8s-cluster/cluster.tf
@@ -10,7 +10,7 @@ image = "xenial-isg-docker-c52f7acc02b0c11d41b174707e4c271b16f52996"
 ssh_user = "ubuntu"
 
 # 0|1 bastion nodes
-number_of_bastions = 0
+number_of_bastions = 1
 flavor_bastion = "8002"
 
 # standalone etcds
@@ -24,8 +24,8 @@ number_of_k8s_masters_no_floating_ip_no_etcd = 0
 flavor_k8s_master = "8002"
 
 # nodes
-number_of_k8s_nodes = 2
-number_of_k8s_nodes_no_floating_ip = 4
+number_of_k8s_nodes = 0
+number_of_k8s_nodes_no_floating_ip = 2
 flavor_k8s_node = "8002"
 
 # GlusterFS
