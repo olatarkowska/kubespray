1d8f317cd44fcad1d105dceddf9bf86b3ea5a693
Anton Khodak <anton.khodak@ukr.net>
2019-01-24 16:51:13 +0000

    Batch of changes

---
+++ sanger/jupyter/commit.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd /Users/ak27/programming/cellgeni/kubespray/sanger/sites
+git add sanger/sites/jupyter-github-auth.yaml && git commit -m "Add new users" && git push
+cd -
---
+++ sanger/jupyter/test-upgrade.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# upgrades newest jupyter installation
+kubectl config use-context large-cellgeni
+ssh -i ~/.ssh/farm4-head1-id_rsa -o ServerAliveInterval=5 -o ServerAliveCountMax=1 -l ubuntu -Nf -L 16700:10.0.3.15:6443 172.27.18.144
+helm upgrade jptt jupyterhub/jupyterhub --namespace jptt --version 0.7.0 --values jupyter-test.yaml
+# helm upgrade --install jptt jupyterhub/jupyterhub --namespace jptt --version 0.7.0 --values jupyter-test.yaml
